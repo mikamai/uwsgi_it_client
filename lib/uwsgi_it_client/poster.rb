18ee36f49254ee383249da9d92d01d4b6397e949
andrea longhi <andrea@spaghetticode.it>
2014-01-14 16:56:48 +0100

    working Poster class

--- lib/uwsgi_it_client/poster.rb
+++ lib/uwsgi_it_client/poster.rb
@@ -1,11 +1,10 @@
 class Poster
   include HTTParty
-  format :json
 
   attr_reader :result
 
   def initialize(url, body, auth_data)
-    @result = post url, body: body, basic_auth: auth_data
+    @result = post url, body: body.to_json, basic_auth: auth_data
   end
 
   private
--- spec/lib/uwsgi_it_client/poster_spec.rb
+++ spec/lib/uwsgi_it_client/poster_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 class UwsgiItClient
   describe Poster do
     it 'requires url, body and auth data' do
-      expect { Response.new }.to raise_error ArgumentError
+      expect { Poster.new }.to raise_error ArgumentError
     end
   end
 end
