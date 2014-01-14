4ad01bd0ed29cd27bda4bfe7a847c53a7f84ad68
andrea longhi <andrea@spaghetticode.it>
2014-01-14 10:46:15 +0100

    add UwsgiItClient initialization code and spec

--- lib/uwsgi_it_client.rb
+++ lib/uwsgi_it_client.rb
@@ -3,7 +3,9 @@ require "uwsgi_it_client/version"
 class UwsgiItClient
   attr_reader :username, :password, :domain
 
-  def initialize(username, password, domain)
-    @username, @password, @domain = username, password, domain
+  def initialize(opts)
+    @domain   = opts.fetch :domain
+    @username = opts.fetch :username
+    @password = opts.fetch :password
   end
 end
--- spec/lib/uwsgi_it_client_spec.rb
+++ spec/lib/uwsgi_it_client_spec.rb
@@ -1,4 +1,27 @@
 require 'spec_helper'
 
 describe UwsgiItClient do
+  let(:username)  { double }
+  let(:password)  { double }
+  let(:domain)    { double }
+
+  subject { UwsgiItClient.new username: username, password: password, domain: domain }
+
+  context 'when the option param is missing' do
+    it { expect { UwsgiItClient.new }.to raise_error ArgumentError }
+  end
+
+  context 'when a required option is missing' do
+    it 'raises an error' do
+      expect { UwsgiItClient.new username: username }.to raise_error
+    end
+  end
+
+  context 'when passing valid options' do
+    it 'sets username, password, domain attributes as expected' do
+      subject.domain.should   == domain
+      subject.password.should == password
+      subject.username.should == username
+    end
+  end
 end
